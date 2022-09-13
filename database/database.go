bc5a4c3fadfe924a991388e8851bb2eff298161b
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-13 14:30:18 +0300

    Update database.go

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"os"
+	"tempsensebackend/model"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,13 +11,14 @@ import (
 
 // function name has to be uppercase in order it to be exported
 func Connect() *gorm.DB {
+
 	host := os.Getenv("HOST")
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
-	db := os.Getenv("DB")
+	database := os.Getenv("DB")
 	port := os.Getenv("PORT")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, db, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, database, port)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
@@ -24,7 +26,7 @@ func Connect() *gorm.DB {
 		fmt.Printf("Error connecting to db")
 	}
 
-	db.AutoMigrate(&model.temperature{})
+	db.AutoMigrate(&model.Temperature{})
 
 	return db
 }
