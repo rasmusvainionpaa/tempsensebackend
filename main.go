970b6962714742d6c3eb2c4f5348cfdadedfe57e
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-13 10:07:11 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"tempsensebackend/routes"
+	"tempsensebackend/controller/temperaturecontroller"
+	"tempsensebackend/database"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	router := gin.Default()
 
+	// Connect to database
+	db := database.Connect()
+
+	// load env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Printf("Error loadin env file")
+	}
+
 	// adds Temperature routes
-	routes.RegisterTemperaturesRoutes(router)
+	temperaturecontroller.RegisterTemperaturesRoutes(router, db)
 
 	// gets run when requested route isn't found
-	router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{}) })
+	router.NoRoute(func(ctx *gin.Context) { ctx.JSON(http.StatusNotFound, gin.H{"Error": "404"}) })
 
 	//starts server
 	router.Run(":8000")
