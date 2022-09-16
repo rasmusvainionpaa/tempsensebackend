41c7707e67265494e454e965a54f58860eeff25c
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-16 12:49:55 +0300

    Create get_temperatures.go

---
+++ src/controller/temperaturecontroller/get_temperatures.go
@@ -0,0 +1,19 @@
+package temperaturecontroller
+
+import (
+	"net/http"
+	"tempsensebackend/src/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h handler) GetTemperatures(c *gin.Context) {
+	var temps []model.Temperature
+
+	if result := h.DB.Find(&temps); result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+
+	c.JSON(http.StatusOK, &temps)
+}
