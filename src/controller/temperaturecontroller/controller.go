188628a4028bf7c62ff65e6dcae6f8f02c1611b3
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-16 12:49:51 +0300

    Create controller.go

---
+++ src/controller/temperaturecontroller/controller.go
@@ -0,0 +1,21 @@
+package temperaturecontroller
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type handler struct {
+	DB *gorm.DB
+}
+
+func RegisterTemperaturesRoutes(gin *gin.Engine, db *gorm.DB) {
+	h := &handler{
+		DB: db,
+	}
+
+	routes := gin.Group("/temperatures")
+	routes.GET("/", h.GetTemperatures)
+	routes.GET("/:id", h.GetTemperature)
+	routes.POST("/", h.PostTemperature)
+}
