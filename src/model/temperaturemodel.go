de4c4a13c89b97539c75475b884ea3aadea97a5c
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-09-17 22:09:33 +0300

    Update temperaturemodel.go

--- src/model/temperaturemodel.go
+++ src/model/temperaturemodel.go
@@ -5,7 +5,9 @@ import (
 )
 
 type Temperature struct {
-	gorm.Model         // creates a gorm model that adds id and creation time
-	Outside    float32 `json:"outside"`
-	Inside     float32 `json:"inside"`
+	gorm.Model                 // creates a gorm model that adds id and creation time
+	OutsideTemperature float32 `json:"OutsideTemperature"`
+	OutsideHumidity    float32 `json:"OutsideHumidity"`
+	InsideTemperature  float32 `json:"InsideTemperature"`
+	InsideHumidity     float32 `json:"InsideHumidity"`
 }
